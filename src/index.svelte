e1f9305f026ff17177f7db2e3042c7021142354a
chooomedia <9967192+chooomedia@users.noreply.github.com>
2024-11-01 23:22:34 +0100

    dasdasdasdsa

--- src/index.svelte
+++ src/index.svelte
@@ -56,10 +56,6 @@
     <meta property="og:url" content="https://keymoji.wtf">
     <meta property="og:site_name" content="{content[$currentLanguage].index.pageTitle || 'Emoji Passwort Generator'}">
     <meta property="og:updated_time" content={updatedTime}>
-    <meta property="og:image" content="https://keymoji.wtf/images/keymoji-social-media-banner-10-2024-min.png">
-    <meta property="og:image:secure_url" content="https://keymoji.wtf/images/keymoji-social-media-banner-10-2024-min.png">
-    <meta property="og:image:width" content="1640">
-    <meta property="og:image:height" content="924">
     <meta property="og:image:alt" content="{content[$currentLanguage].index.pageDescription || '🔑 Passwords reimagined. 🎯 Uncrackable emoji passwords. 🌈 Free. Secure. Innovative. 🤖 AI-resistant technology. 🌍 Available in 15+ languages.'}">
     <meta property="og:image:type" content="image/png">
 
