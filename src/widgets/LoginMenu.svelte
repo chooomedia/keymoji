16e4c0855cd775c1cfc84ed49773c785f4bdb8f0
chooomedia <9967192+chooomedia@users.noreply.github.com>
2024-11-18 10:51:41 +0100

    extended manifest & changed wording github button

--- src/widgets/LoginMenu.svelte
+++ src/widgets/LoginMenu.svelte
@@ -7,5 +7,5 @@
 </button>
 
 <button aria-label="have a look of the code from keymoji on github" class="fixed top-5 right-5 hidden md:block bg-powder text-black dark:bg-aubergine-dark dark:text-powder py-3.5 px-4 rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none border-4 border-creme dark:border-aubergine">
-    <a class="hidden md:block" href="https://github.com/chooomedia/keymoji" target="_blank" aria-label="Star chooomedia/keymoji on GitHub">⭐ Stare on Github</a>
+    <a class="hidden md:block" href="https://github.com/chooomedia/keymoji" target="_blank" aria-label="Star chooomedia/keymoji on GitHub">⭐ Star on Github</a>
 </button>
