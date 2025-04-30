73fa45117f91b754f5a9bdcc8c3100d13f87abec
chooomedia <9967192+chooomedia@users.noreply.github.com>
2025-04-30 20:19:46 +0200

    berl

--- backend/api/contact.js
+++ backend/api/contact.js
@@ -65,9 +65,7 @@ async function sendBrevoEmail({ name, email, message, emailContent }) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>${
-            emailContent.title || 'Keymoji - Emoji Password Generator'
-        }</title>
+        <title>Keymoji - Emoji Password Generator</title>
         <!--[if mso]>
         <noscript>
             <xml>
