78aebcd921c549958994a9d35d11da2de3763b86
chooomedia <9967192+chooomedia@users.noreply.github.com>
2025-05-01 17:10:22 +0200

    fixed version wording index

--- src/index.svelte
+++ src/index.svelte
@@ -82,7 +82,7 @@
                 type="button"
                 aria-label="View version history"
               >
-                {getText('header.pageVersion') || 'v0.2.2'}
+                v{getText('header.pageVersion') || 'v0.4.2'}
               </button>
               <span class="px-2">·</span>
               <span>Created by</span>
